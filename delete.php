574d0f6888cb526ec67b61df018bf533e3d5cc87
HotPizzaYT <57576298+HotPizzaYT@users.noreply.github.com>
2021-07-24 00:19:15 -0500

    Add account delete

--- delete.php
+++ delete.php
@@ -7,7 +7,7 @@ if(isset($_SESSION["username"]) && isset($_SESSION["email"])){
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <h1>Deleting your account</h1>
 <p>Are you sure you want to delete account "<?php echo $_SESSION["username"]; ?>" with email "<?php echo $_SESSION["email"] ?>"?</p>
-<p><a href="javascript:alert('Not implemented'); void(0)">Yes</a> - <a href="javascript:alert('Not implemented'); void(0)">No</a>
+<p><a href="reallyDelete.php">Yes</a> - <a href="acc.php">No</a>
 <?php
 } else {
 header("Location: index.php");
